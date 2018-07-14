587245546ec68fb15fa2e912fe4b3f6aff777831
HanGyo Jeong <hangyojeong@HanGyoui-MacBook-Pro.local>
2018-07-14 17:03:48 +0900

    feat: Make CNN DeepLearning

--- CNN/caltech101_keras.py
+++ CNN/caltech101_keras.py
@@ -6,5 +6,44 @@ import numpy as np
 categories = ["chair", "camera", "butterfly", "elephant", "flamingo"]
 nb_classes = len(categories)
 
+#Set the image size
 image_w = 64
 image_h = 64
+
+#Open the Data
+X_train, X_test, y_train, y_test = np.load("./image/5obj.npy")
+#Regulate the Data
+X_train = X_train.astype("float") / 256
+X_test = X_test.astype("float") / 256
+print('X_train shape:', X_train.shape)
+
+#Construct the Model
+model = Sequential()
+model.add(Convolution2D(32,3,3,border_mode='same',input_shape=X_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same'))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+#Train the model
+model.fit(X_train, y_train, batch_size=32, nb_epoch=50)
+
+#Estimate the model
+score = model.evaluate(X_test, y_test)
+print('loss=',score[0])
+print('accuracy=',score[1])
